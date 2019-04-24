fb4bb32656431c87aef8b31c51dffd7047de0044
himam4@uic.edu <hammadimam99@gmail.com>
2019-04-24 16:34:19 -0500

    working prototype of code that scans and sorts properly

--- Code/me250_proj3_code/me250_proj3_code.ino
+++ Code/me250_proj3_code/me250_proj3_code.ino
@@ -21,8 +21,10 @@ int stepspeed = 40;                                       //speed that the stepp
 float sense[3];                                           //initialize the 3 values put out by the TCS
 int blinknum = 5;                                         //how many times the LED will blink when signalling (unless custom number used)
 int servopos[] = {35, 70, 105, 140};                      //locations of the bins
-String color = "none ";                                      //initialize color to none to use as placeholder
-int ballsLeft = 12;                                       //this counts how many balls need to be processed, should be 0 when all balls are done
+String color = "none ";                                   //initialize color to none to use as placeholder
+int ballsTotal = 17;                                      //total balls to be sorted (used to make sure no balls get left in hopper/device)  
+int ballsToSort = 12;                                     //this counts how many balls need to be processed, should be 0 when all balls are done
+int range = 10;                                           //range from "centerpoint" defined in struct that a ball can be 
 
 //initialize colors
 //make it easier for status updates using one of these 'default' colors
@@ -39,37 +41,37 @@ int RGBoff[] = {0, 0, 0,};            //used for turning the led off
 
 struct ballColor{
   String color;
-  int thresh[6];
+  int thresh[3];
   int RGB[3];
 } ;
 
 ballColor orange = {
   "orange",
-  {105, 125, 60, 80, 50, 70},
+  {105, 80, 60},
   {255, 165, 0}
 };
 
 ballColor yellow = {
   "yellow",
-  {58, 68, 96, 110, 65, 75},
+  {65, 105, 70},
   {200, 255, 0}
 };
 
 ballColor green = {
   "green",
-  {55, 75, 95, 105, 70, 80},
+  {70, 90, 70},
   {0, 255, 0}
 };
 
 ballColor blue = {
   "blue",
-  {41, 51, 90, 100, 95, 105},
+  {55, 95, 90},
   {0, 255, 255}
 };
 
 ballColor pink = {
   "pink",
-  {150, 160, 42, 52, 41, 71},
+  {120, 60, 60},
   {170, 0, 255}
 };
 
@@ -99,7 +101,7 @@ void setup() {
   
   //initialize servo motor
   servo.attach(servopin);
-  servo.write(90);
+  servo.write(servopos[3]); 
  
   //initialize DC motor
   stpr.setSpeed(stepspeed);
@@ -139,8 +141,8 @@ void setup() {
 
 void loop() {
   stpr.step(683);           //stepper motor rotates 120deg (IF WE DO 3 HOLES, WE WILL HAVE 0.3 DEGREE OFF FOR EACH BALL
-  delay(1500);              //waits 1.5 seconds just to give shit time to work
-  servo.write(servopos[3]);                   //move the servo to the trash bin
+  Serial.println("\n\nRotating wheel.");
+  delay(500);              //waits 1.5 seconds just to give shit time to work
   
   //color sensor and its read out
   tcs.setInterrupt(false);                        //turn on LED
@@ -148,47 +150,51 @@ void loop() {
   tcs.getRGB(&sense[0], &sense[1], &sense[2]);    //assign the read values to the array sense[]
   //tcs.setInterrupt(true);                         //
 
-  colorBlink(RGBred, 3);
+  //colorBlink(RGBred, 3);
 
   //check color of the ball
   for(int i=0; i<5; i++){                       //for every color possible
     //Serial.println(balls[i].color);
     if( (checkColor(balls[i].thresh, sense)) ){      //if the sensed color is within the threshold
         color = balls[i].color;                  //set the color string to the name of the color
-        Serial.println(String(color));
         //colorBlink(balls[i].RGB, 3);         //blink the led with the sensed color
         break;                                      //exit the for loop if one is found
     }
    else{
-      color = "none ";
-      colorBlink(RGBred, 1);
+      color = "none";
+      //colorBlink(RGBred, 1);
     }
   }
 
   //print to serial monitor
-  Serial.println("R: " + String(sense[0]) + "\t G: " + String(sense[1]) + "\t B:" + String(sense[2])); //print the RGB values to the serial monitor
-  Serial.println(String(color) + "\n");                          //print the sensed color to the serial monitor
+  Serial.println("\nR: " + String(sense[0]) + "\t G: " + String(sense[1]) + "\t B:" + String(sense[2])); //print the RGB values to the serial monitor
+  Serial.println("Ball detected: " + String(color) + "\n");                          //print the sensed color to the serial monitor
 
   //routing the ball
   [&]{
-  for(int i = 0; i<3; i++){                   //for each bin (3 total)
-    for(int j = 0; j<4; j++){                   //for each color desired in the bin (3 total)
-      Serial.println("Bin: " + String((i+1)));
-      Serial.println( String(bin[i].item[j]) + "\t" + String(color) );
-      if(String(bin[i].item[j]) == String(color)){                //if the sensed ball matches one of the colors
-        //servo.write(servopos[i]);                   //move the servo to the bin
-        bin[i].item[j] = "x";                        //"cross off" the ball that was guided by changing the array value to null 
-        Serial.println("Bin " + String(i+1) + ", item " + String(j) + " erased."); 
-        ballsLeft--;                                //reduce number of balls left by 1
-        return;                                      //exit both for loops 
+    for(int i = 0; i<3; i++){                   //for each bin (3 total)
+      for(int j = 0; j<4; j++){                   //for each color desired in the bin (3 total)
+        Serial.println("Bin " + String((i+1)) + ", Item " + String(j+1) + " is " + bin[i].item[j]);
+        if(String(bin[i].item[j]) == String(color)){                //if the sensed ball matches one of the colors
+          servo.write(servopos[i]);                   //move the servo to the bin
+          Serial.println("Servo set to bin " + String(i+1));
+          bin[i].item[j] = "done";                        //"cross off" the ball that was guided by changing the array value to null 
+          ballsToSort--;                                //reduce number of balls left by 1
+          Serial.println("Bin " + String(i+1) + ", item " + String(j+1) + " done. \nBalls Left: " + String(ballsToSort)); 
+          return;                                      //exit both for loops 
+        }                                       
       }
-      /*else{                                       //else (if the sensed ball does NOT match a color in the list)
-        Serial.println("Trash");
-      }
-      */
     }
-  }
+    servo.write(servopos[3]);                   //move the servo to the trash bin
+    Serial.println("Servo set to trash");                    //display that the servo has been moved to the trash
+    return;                                     //loop back
+  
   }();
+
+  if(ballsToSort ==0){
+    Serial.println("\n\nDone.");
+    while(1);
+  }
 }
 
 //function to write the color to a 
@@ -211,14 +217,10 @@ void colorBlink(int list[], int n){
 int checkColor(int colorList[], float sensedList[]){
   int sum = 0;
     for(int i=0; i<3; i++){
-      //Serial.println(String(colorList[(2*i)]) + "\t" + String(colorList[(2*i +1)]) + "\t" + String(sensedList[i]) 
-      //+ "\t" + String((colorList[(2*i)] <= sensedList[i] && sensedList[i] <= colorList[(2*i + 1)])));
-      if(colorList[(2*i)] <= sensedList[i] && sensedList[i] <= colorList[(2*i + 1)]){
+      if( abs(colorList[i]-sensedList[i]) < range ) {
         sum++;
     }
   }
-
-  Serial.println(String(sum));
   
   if(sum == 3){
     return true;
