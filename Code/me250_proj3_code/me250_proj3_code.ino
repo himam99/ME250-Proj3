e055fd3adfc320ffa575e63395a0e0c37a89e019
Hammad Imam <himam4@uic.edu>
2019-05-03 11:35:43 -0500

    changed code

--- Code/me250_proj3_code/me250_proj3_code.ino
+++ Code/me250_proj3_code/me250_proj3_code.ino
@@ -10,13 +10,14 @@ Servo servo;
 Adafruit_TCS34725 tcs =Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
 
 //CONFIG
+
 //ALL PINS GO HERE
 int red = 11;                         //
 int grn = 10;                         //pins for RGBLED
 int blu = 9;                          //
-Stepper stpr(512, A0, A2, A1, A3);    //in the order IN1, 3, 2, 4
+Stepper stpr(512, A0, A2, A1, A3);    //in the order in1, 3, 2, 4
 int servopin = 3;                     //servo pin
-//TCS: SDA -> A4, SCL -> A5.          //This can't be changed (unless we really wanted to but we don't)
+//TCS: SDA -> A4, SCL -> A5.  This can't be changed (unless we really wanted to but we don't)
 
 //initialize variables
 int stepspeed = 40;                                       //speed that the stepper operates
@@ -28,6 +29,7 @@ int ballsTotal = 17;                                      //total balls to be so
 int ballsToSort = 12;                                     //this counts how many balls need to be processed, should be 0 when all balls are done
 int range = 10;                                           //range from "centerpoint" defined in struct that a ball's RGB can be
 
+
 //initialize colors
 //make it easier for status updates using one of these 'default' colors
 //less bright than other colors displayed (in theory)
@@ -35,7 +37,8 @@ int RGBwhite[] = {255, 255, 255};     //used for "done" (would pick green but it
 int RGBred[] = {255, 0, 0};           //used for "not done" or alerts or whatever
 int RGBoff[] = {0, 0, 0,};            //used for turning the led off 
 
-//define ball color struct with name of color, threshold for sensing, and color for LED:
+
+//ball color struct with name of color, threshold for sensing, and color for LED:
 //row 1: String name of color
 //row 2: array {red, grn, blu} for what the ball color should show up as
 //row 3: array {red, grn, blu} color to send to rgbled
@@ -46,7 +49,6 @@ struct ballColor{
   int RGB[3];
 } ;
 
-//assign values to structs for each ball color
 ballColor orange = {
   "orange",
   {105, 80, 60},
@@ -93,6 +95,13 @@ bin_type binContent = {
   {"pink", "pink", "yellow", "blue"}
 };
 
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//!!!!!IF TESTING DO NOT CHANGE ANYTHING BELOW THIS POINT!!!!!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
 void setup() {
   //initialize serial line
   Serial.begin(9600);
@@ -106,12 +115,12 @@ void setup() {
 
   //initialize pins
   pinMode (red, OUTPUT);
-  pinMode (grn, OUTPUT);
   pinMode (blu, OUTPUT);
+  pinMode (grn, OUTPUT);
   pinMode (A4, INPUT);
   pinMode (A5, INPUT);
 
-  //Make sure color sensor initializes
+  //for color sensor
   if (tcs.begin()) {
     Serial.println("Found TCS");} 
   else {
@@ -120,7 +129,6 @@ void setup() {
   }
   
   //Create an array of structs called "balls" from the previously generated structs.
-  //Used so that balls can be checed using nested for loops instead of being searched one by one
   balls[0] = orange;
   balls[1] = yellow;
   balls[2] = green;
@@ -134,23 +142,24 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("\n\nRotating wheel.");              //write to the serial monitor that the wheel is rotating
-  
-  if( (ballsTotal%3) == 0){                           //finds modulo 3 of balls to sort (possibilities are 0, 1, or 2)
+  if( (ballsTotal%3) == 0){                          //finds modulo 3 of balls to sort (possibilities are 0, 1, or 2)
       stpr.step(682);                                 //if it's 0, rotate by 120 deg (rounded down)
   }                                                   
   else{
     stpr.step(683);                                   //otherwise rotate by 120 deg (rounded up)
   }                                                   //this part makes sure that it stays 360 deg every time without drifting
+
+  Serial.println("\n\nRotating wheel.");              //write to the serial monitor that the wheel is rotating
   
   //color sensor and its read out
   tcs.setInterrupt(false);                            //turn on LED for TCS
   delay(50);                                          //take 50ms to read
   tcs.getRGB(&sense[0], &sense[1], &sense[2]);        //assign the read values to the array sense[]
-  tcs.setInterrupt(true);                             //honestly not sure about this part but it was in example code or somethin           
+  tcs.setInterrupt(true);                       
 
-  //Checking the color of the ball
   colorBlink(RGBred, 3);                              //blink the led to show that the scan is starting
+
+  //check color of the ball
   for(int i=0; i<5; i++){                             //for every color possible
     Serial.println(balls[i].colorName);               //display the color being checked to the serial monitor
     if( (checkColor(balls[i].color, sense)) ){        //if the sensed color matches a ball (see below for details on checkColor function)
@@ -167,27 +176,28 @@ void loop() {
     }
   }
 
-  //Printing what was detected to serial monitor
+  //print to serial monitor
   Serial.println("\nR: " + String(sense[0]) + "\t G: " + String(sense[1]) + "\t B:" + String(sense[2]));  //print the RGB values to the serial monitor
   Serial.println("Ball detected: " + String(color) + "\n");                                               //print the sensed ball to the serial monitor
 
-  //Routing the ball to the correct bin/trash
+  //routing the ball
   [&]{
     for(int i = 0; i<3; i++){                                                                           //for each bin (3 total)
       for(int j = 0; j<4; j++){                                                                         //for each spot in the bin (4 total)
-        Serial.println("Bin " + String((i+1)) + ", Item " + String(j+1) + " is " + bin[i].item[j]);     //write what is in that spot (total of 12 spots)
+        Serial.println("Bin " + String((i+1)) + ", Item " + String(j+1) + " is " + bin[i].item[j]);     //write what is in that spot
         if(String(bin[i].item[j]) == String(color)){                                                    //if the sensed ball matches one of the colors
           servo.write(servopos[i]);                                                                     //move the servo to the bin
           Serial.println("Servo set to bin " + String(i+1));                                            //display what bin the servo is set to
           bin[i].item[j] = "done";                                                                      //"cross off" the spot by changing the array value to "done"
           ballsToSort--;                                                                                //subtract one from the number of balls sorted
-          Serial.println("Bin " + String(i+1) + ", item " + String(j+1) + " done.");                    //display the spot in the bin is complete
-          Serial.println("Balls Left to Sort: " + String(ballsToSort));                                 //display # of balls left to sort
+          Serial.println("Bin " + String(i+1) + ", item " + String(j+1) + " done."); 
+          Serial.println("Balls Left to Sort: " + String(ballsToSort));                                 //display the spot in the bin is complete, display # of balls left to sort
           colorBlink(RGBwhite, 3);                                                                      //flash that a ball has been sorted correctly
-          return;                                                                                       //exit both for loops by exiting the [&] flag
+          return;                                                                                       //exit both for loops 
         }                                       
-      }                                         //"else" if the sensed ball does not match any of the required balls
-    }                                           //(if the conditional is never true it doesn't leave the [&] flag before this part runs)                      
+      }
+    }
+                                                //"if" nothing has been found in the for loops
     servo.write(servopos[3]);                   //move the servo to the trash bin
     Serial.println("Servo set to trash");       //display that the servo has been moved to the trash
     return;                                     //exit
@@ -195,18 +205,29 @@ void loop() {
 
   Serial.println("Balls left in hopper: " + String(ballsTotal));  //display how many balls are left in the hopper
 
-  if( (ballsToSort==0) && (ballsTotal == 0)){         //if all balls have been sorted and there are no balls left in the hopper
-    Serial.println("\n\nDone.");                      //print that the sorting is done
-    colorWrite(RGBwhite);                             //turn the LED white
-    while(1);                                         //stop running
+  if( (ballsToSort==0) && (ballsTotal == 0)){         //if all balls have been sorted and there are no balls 
+    Serial.println("\n\nDone.");                //print that the sorting is done
+    colorWrite(RGBwhite);                       //turn the LED white
+    while(1);                                   //stop running
   }
 }
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
 //FUNCTIONS USED IN CODE ABOVE 
 
 >>>>>>> parent of b5b4068... minor changes to CAD and code
+=======
+/*
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$$$$$$$$$FUNCTIONS USED IN CODE ABOVE$$$$$$$$$$ 
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$$$$$$$$$$$$$DO NOT CHANGE THESE$$$$$$$$$$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+*/
+
+>>>>>>> parent of a7b54f3... Fixed comments in code
 //function to write the color from a list to a RGBLED
 void colorWrite(int list[]){    //takes a list of 3 values to write to LED
   analogWrite(red, list[0]);
@@ -227,10 +248,10 @@ void colorBlink(int list[], int n){   //takes a list of 3 values to write to LED
 
 //function to check the color of a ball
 int checkColor(int colorList[], float sensedList[]){    //takes a list of what colors should be sensed and a list of what colors are sensed
-  int sum = 0;                                          ///for each of 3 colors
+  int sum = 0;                                          //initialize a sum to 0    
+  for(int i=0; i<3; i++){                               //for each of 3 colors
     if( abs(colorList[i]-sensedList[i]) < range ) {     //if the "should be" color is within range of the "sensed" color
-        sum++;                                       initialize a sum to 0    
-  for(int i=0; i<3; i++){                               /   //add one to the sum
+        sum++;                                          //add one to the sum
     }
   }
   if(sum == 3){                                         //if the sum is 3 (true for all 3 of RGB)
@@ -240,3 +261,26 @@ int checkColor(int colorList[], float sensedList[]){    //takes a list of what c
     return false;                                       //return false
   }
 }
+
+
+/*
+ ________                                         __    ________ 
+/        |                                      _/  |  /        |
+$$$$$$$$/______    ______   _____  ____        / $$ |  $$$$$$$$/ 
+   $$ | /      \  /      \ /     \/    \       $$$$ |      /$$/  
+   $$ |/$$$$$$  | $$$$$$  |$$$$$$ $$$$  |        $$ |     /$$/   
+   $$ |$$    $$ | /    $$ |$$ | $$ | $$ |        $$ |    /$$/    
+   $$ |$$$$$$$$/ /$$$$$$$ |$$ | $$ | $$ |       _$$ |_  /$$/     
+   $$ |$$       |$$    $$ |$$ | $$ | $$ |      / $$   |/$$/      
+   $$/  $$$$$$$/  $$$$$$$/ $$/  $$/  $$/       $$$$$$/ $$/ 
+    __       __  ________         ______   _______    ______  
+/  \     /  |/        |       /      \ /       |  /      \ 
+$$  \   /$$ |$$$$$$$$/       /$$$$$$  |$$$$$$$/  /$$$$$$  |
+$$$  \ /$$$ |$$ |__          $$____$$ |$$ |____  $$$  \$$ |
+$$$$  /$$$$ |$$    |          /    $$/ $$      \ $$$$  $$ |
+$$ $$ $$/$$ |$$$$$/          /$$$$$$/  $$$$$$$  |$$ $$ $$ |
+$$ |$$$/ $$ |$$ |_____       $$ |_____ /  \__$$ |$$ \$$$$ |
+$$ | $/  $$ |$$       |      $$       |$$    $$/ $$   $$$/ 
+$$/      $$/ $$$$$$$$/       $$$$$$$$/  $$$$$$/   $$$$$$/  
+                                                                                                                                                                                                                                                                
+ */
